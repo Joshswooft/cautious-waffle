b55e34fbda1e8777f672e746af2a811c378d95bd
Josh Dando <joshdando@joshs-mbp.home>
2021-05-09 23:35:30 +0100

    chore: add tests for package class

---
+++ __tests__/package.test.ts
@@ -0,0 +1,57 @@
+import { NodeType, Package } from "../src/package";
+
+describe('Package', () => {
+    test('should create an empty package', () => {
+        const p = new Package();
+        expect(p.getNodes().length).toBe(0);
+    });
+
+    test('should return list of selected packages', () => {
+        const p = new Package();
+
+        p.insert({name: 'A', selected: true});
+        p.insert({name: 'B', selected: false});
+        p.insert({name: 'C', selected: true});
+
+        expect(p.StringSlice()).toMatchObject(['A', 'C']);
+
+    });
+
+    test('adding 2 packages with the same name should fail', () => {
+        const p = new Package();
+        const testPackage: NodeType = { name: 'test' };
+        p.insert(testPackage);
+        expect(() => p.insert(testPackage)).toThrow();
+    })
+
+    test('should set a new package', () => {
+        const p = new Package();
+
+        p.Toggle({ name: "test"});
+
+        expect(p.StringSlice()).toMatchObject(['test']);
+    });
+
+    test('should unset a package', () => {
+        const p = new Package();
+        const name = "test";
+
+        p.Toggle({ name });
+        p.Toggle({name })
+
+        expect(p.getNodes().length).toBe(1);
+        expect(p.StringSlice()).toMatchObject([]);
+    });
+
+    test('should set selected to true for an existing un-selected package', () => {
+        const p = new Package();
+        const name = "test";
+
+        p.insert({name, selected: false});
+        p.Toggle({ name });
+
+        expect(p.getNodes().length).toBe(1);
+        expect(p.StringSlice()).toMatchObject([name]);
+
+    });
+})
--- src/package.ts
+++ src/package.ts
@@ -1,19 +1,43 @@
 import { DirectedGraph } from "typescript-graph";
-import * as hash from 'object-hash';
+
 export type NodeType = { name: string, conflicts?: Array<string>, selected?: boolean }
 
 export class Package extends DirectedGraph<NodeType> {
 
-    constructor(nodeIdentity: (node: NodeType) => string = (node) => hash(node)) {
-        super(nodeIdentity);
+    constructor(nodeIdentity?) {
+        if (!nodeIdentity) {
+            super((n: NodeType) => n.name)
+        }
+        else {
+            super(nodeIdentity);
+        }
     }
 
+    /**
+     * 
+     * @returns a list of selected packages
+     */
     StringSlice(): string[] {
         return this.getNodes().filter(n => n.selected === true).map(n => n.name)
     }
 
+    /**
+     * 
+     * @param p package
+     * 
+     * Adds/removes the package from the graph depending on its selected status
+     * 
+     */
     Toggle(p: NodeType) {
-        this.upsert(p);
+        const matched = this.getNodes().find(n => n.name === p.name);
+        if (matched) {
+            matched.selected = !matched.selected;
+            this.upsert(matched);
+        }
+        else {
+            p.selected = true;
+            this.insert(p);
+        }
     }
 
 }
