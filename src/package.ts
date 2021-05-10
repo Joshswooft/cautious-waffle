fcc8362e08ed9e6257dee09c7f005dd0698d8c59
Josh Dando <joshdando@joshs-mbp.home>
2021-05-10 03:09:30 +0100

    chore: remove un-used comment

--- src/package.ts
+++ src/package.ts
@@ -30,10 +30,6 @@ export class Package extends DirectedGraph<NodeType> {
      * 
      */
     Toggle(p: NodeType) {
-        /*
-         TODO: if the package is being selected then see if that will cause the graph to be incoherent
-         then remove the dependencies which are causing the conflicts
-         */
         const matched = this.getNodes().find(n => n.name === p.name);
         if (matched) {
             matched.selected = !matched.selected;
