cb7e6f53131da48ba8c3e173130ab5bb10ddf3f6
Josh Dando <joshdando@joshs-mbp.home>
2021-05-10 01:26:44 +0100

    chore: added compare node types set

--- __tests__/test-utils.test.ts
+++ __tests__/test-utils.test.ts
@@ -1,25 +1,54 @@
-import { setsEqual } from "../src/test-utils";
+import { NodeType } from "../src/package";
+import { arraysEqual, setsEqual } from "../src/test-utils";
 
 describe('test utils', () => {
-    describe('setsEqual', () => {
+    describe('arraysEqual', () => {
         test('two arrays should not be equal if they are different length', () => {
             const a = ['a'];
             const b = ['a', 'b'];
 
-            expect(setsEqual(a, b)).toBe(false);
+            expect(arraysEqual(a, b)).toBe(false);
         })
 
         test('two arrays with the same length but different elements will be false', () => {
             const a = ['a'];
             const b = ['b'];
 
-            expect(setsEqual(a, b)).toBe(false);
+            expect(arraysEqual(a, b)).toBe(false);
         })
 
         test('should return true for a matching array', () => {
             const a = ['a', 'b', 'c'];
 
-            expect(setsEqual(a, a)).toBe(true);
+            expect(arraysEqual(a, a)).toBe(true);
         })
     })
+
+    describe('setsEqual', () => {
+        test('2 sets with the same obj keys/values should be equal if they are also the same length', () => {
+            const a: NodeType[] = [{ name: 'bar' }];
+            
+            expect(setsEqual(a, a)).toBe(true);
+        });
+
+        test('2 sets with different lengths should be false', () => {
+            const a: NodeType[] = [{ name: 'bar' }];
+            const b: NodeType[] = [{ name: 'bar' }, { name: 'foo'}];
+
+            expect(setsEqual(a, b)).toBe(false);
+        })
+
+        test('2 sets with different property values should be false', () => {
+            const a: NodeType[] = [{ name: 'bar' }];
+            const b: NodeType[] = [{ name: 'foo' }];
+
+            expect(setsEqual(a, b)).toBe(false);
+        })
+        test('2 sets should match with name casing being insensitive', () => {
+            const a: NodeType[] = [{ name: 'bar' }];
+            const b: NodeType[] = [{ name: 'BaR' }];
+            
+            expect(setsEqual(a, b)).toBe(true);
+        })
+    });
 })
--- src/test-utils.ts
+++ src/test-utils.ts
@@ -1,4 +1,6 @@
-export function setsEqual(a: string[], b: string[]): boolean {
+import { NodeType } from "./package";
+
+export function arraysEqual(a: string[], b: string[]): boolean {
   var ka = a.sort();
   var kb = b.sort();
   if (ka.length != kb.length) {
@@ -19,4 +21,26 @@ export function setsEqual(a: string[], b: string[]): boolean {
     b: kb,
   });
   return false;
+}
+
+// case insensitive sort on name field
+function compareNodeType(a: NodeType, b: NodeType) {
+  const upperA = a.name.toUpperCase();
+  const upperB = b.name.toUpperCase();
+
+  return (upperA < upperB) ? -1 : (upperA > upperB) ? 1 : 0;
+}
+
+// TODO: replace this with a more future proof approach
+function compareNode(a: NodeType, b: NodeType) {
+  return a.name.toUpperCase() === b.name.toUpperCase() && a.conflicts === b.conflicts && a.selected === b.selected
+}
+
+export function setsEqual(a: NodeType[], b: NodeType[]) {
+  var ka = a.sort(compareNodeType);
+  var kb = b.sort(compareNodeType);
+  if (ka.length != kb.length) {
+    return false;
+  }
+  return ka.every((n, i) => compareNode(n, kb[i]))
 }
